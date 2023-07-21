dd24b30d8964f20800b7b0a3a6cdddcf3a367e66
0xb337r007 <0xe4e5@proton.me>
2023-07-21 10:04:02 +0200

    add deploy script for OwnerToken

--- scripts/deploy_owner_token.ts
+++ scripts/deploy_owner_token.ts
@@ -2,31 +2,24 @@ import { ethers } from "hardhat";
 import { pn } from "./utils";
 
 async function main() {
-  const TestToken = await ethers.getContractFactory("Currency");
-  const testToken = await TestToken.deploy();
-  const ownerTokenAddress = testToken.address;
-  const masterTokenAddress = testToken.address;
-
-  const CollectibleV1 = await ethers.getContractFactory("CollectibleV1");
+  const CollectibleV1 = await ethers.getContractFactory("OwnerToken");
   const contract = await CollectibleV1.deploy(
     "Test",
     "TEST",
-    100,
-    true,
-    true,
     "http://local.dev",
-    ownerTokenAddress,
-    masterTokenAddress
+    "Test 2",
+    "TEST 2",
+    "http://local2.dev",
+    "0x12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678"
   );
 
   const instance = await contract.deployed();
   const tx = instance.deployTransaction;
   const rec = await tx.wait();
   console.log(
-    `CollectibleV1 deployed at ${instance.address}. Gas used: ${pn(
-      rec.gasUsed
-    )}`
+    `OwnerToken deployed at ${instance.address}. Gas used: ${pn(rec.gasUsed)}`
   );
+  console.log("Master token deployed at", rec.events[0].args.masterToken);
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
