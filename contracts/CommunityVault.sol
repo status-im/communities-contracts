649ccb37e75f9535db4763e09f9b56065258eb49
0xb337r007 <0xe4e5@proton.me>
2023-11-27 10:05:05 +0100

    use named import

--- contracts/CommunityVault.sol
+++ contracts/CommunityVault.sol
@@ -2,10 +2,10 @@
 
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "./CommunityOwnable.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { CommunityOwnable } from "./CommunityOwnable.sol";
 
 /**
  * @title CommunityVault
