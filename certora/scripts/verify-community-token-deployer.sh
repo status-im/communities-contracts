e3142f1e52ab404dd296abd891c943e0b4dc7eb1
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2024-01-11 11:30:14 +0100

    refactor: move certora configs into individual scripts
    This is done to allow execution of multiple verifications per command,
    such that we can still use `pnpm verify` as usual to run all specs, but
    also target individual specs using `pnpm verify:sub_command`.

---
+++ certora/scripts/verify-community-token-deployer.sh
@@ -0,0 +1,15 @@
+certoraRun \
+  contracts/CommunityTokenDeployer.sol \
+  contracts/CommunityOwnerTokenRegistry.sol \
+  contracts/factories/CommunityOwnerTokenFactory.sol \
+  contracts/factories/CommunityMasterTokenFactory.sol \
+--verify CommunityTokenDeployer:certora/specs/CommunityTokenDeployer.spec \
+--link CommunityTokenDeployer:deploymentRegistry=CommunityOwnerTokenRegistry \
+--link CommunityTokenDeployer:ownerTokenFactory=CommunityOwnerTokenFactory \
+--link CommunityTokenDeployer:masterTokenFactory=CommunityMasterTokenFactory \
+--packages @openzeppelin=lib/openzeppelin-contracts \
+--optimistic_loop \
+--rule_sanity "basic" \
+--wait_for_results "all" \
+--msg "Verifying CommunityTokenDeployer.sol"
+
