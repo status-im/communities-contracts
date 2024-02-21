c63bc0b7160d59630d9d3ca7a16a59ff8e463744
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2024-02-21 15:59:51 +0100

    cleanup(BaseToken): remove unused import

--- contracts/tokens/BaseToken.sol
+++ contracts/tokens/BaseToken.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import { ERC721Enumerable } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import { Context } from "@openzeppelin/contracts/utils/Context.sol";
 import { Counters } from "@openzeppelin/contracts/utils/Counters.sol";
