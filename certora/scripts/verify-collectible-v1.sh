9194946d1ddb9dc5de4ff67c4b17f4349eda69d6
r4bbit <445106+0x-r4bbit@users.noreply.github.com>
2024-02-21 16:47:31 +0100

    adjust config

--- certora/scripts/verify-collectible-v1.sh
+++ certora/scripts/verify-collectible-v1.sh
@@ -1,7 +1,11 @@
 certoraRun \
   contracts/tokens/CollectibleV1.sol \
   certora/harness/CollectibleV1Harness.sol \
+  contracts/tokens/OwnerToken.sol \
+  contracts/tokens/MasterToken.sol \
 --verify CollectibleV1Harness:certora/specs/CollectibleV1.spec \
+--link CollectibleV1Harness:ownerToken=OwnerToken \
+--link CollectibleV1Harness:masterToken=MasterToken \
 --packages @openzeppelin=lib/openzeppelin-contracts \
 --optimistic_loop \
 --loop_iter 3 \
